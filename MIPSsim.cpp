482b736a1e3d5abd7f853b3478572c06c24ec654
Ubuntu2 <c.avogadro@ufl.edu>
2016-11-18 10:35:42 -0500

    nommore errors, almost done preprocessing

--- MIPSsim.cpp
+++ MIPSsim.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 #include <utility>      // std::pair
 
 
@@ -19,7 +20,7 @@ class Instruc
         int src1;   //store src1 index that will be accesed in the vector containing the 32 registers
         int src2;
         int dest;
-        static pc = 60;
+        static int pc;
         int addrs; //array pos of of instruction address to be accessed ex: lw r5,144(r0)
     Instruc()
     {
@@ -29,11 +30,13 @@ class Instruc
 
 
 
-}
+};
+
+int Instruc::pc = 60;
 
 class Simulator
 {   
-    private:
+    public:
         string instruction;
         string format;
         string opcode;
@@ -45,11 +48,10 @@ class Simulator
         int pc_counter;
         int instr_pos;  //Used to acces and traverse string
         string file_name;
-        vector <Instruc> instructions;  //Stores the instructions
+        vector <Instruc*> instructions;  //Stores the instructions
         vector <int> regs; //Stores the values for the registers
         vector<int> data; //stores the data in memory that will be accesed using lw and sw
 
-    public:
 
     Simulator(string file):file_name{file}{}
     //A preprocessing step that populates the data vector and the instruction vectors with instruction structs that contain details pertaining to each instruction
@@ -63,7 +65,7 @@ class Simulator
         //Read instructions from file while there are instructions to read
         //When the break instruction is reached stop taking instructions
         //Strat taking in data values
-        while(infile>>instruction && Instruction!="00111100000000000000000000001101")
+        while(infile>>instruction && instruction!="00111100000000000000000000001101")
         {
 
             reset();
@@ -118,7 +120,7 @@ class Simulator
                 if (opcode == "000")
                 {
                 
-                    Instruc temp_instruc = new Instruc;
+                    Instruc *temp_instruc = new Instruc;
                     temp_instruc->type = "NOP";
                     temp_instruc->op = opcode;
                     instructions.push_back(temp_instruc);
@@ -126,10 +128,11 @@ class Simulator
                 }
                 else if (opcode == "001")
                 {
-                    Instruc temp_instruc = new Instruc;
+                    Instruc *temp_instruc = new Instruc;
                     temp_instruc->type = "J";
                     //Transfor bit string into a bitset and then into an integer value to store in the address field
-                    temp_instruc->addrs = get_num(bitset<26> temp_addrs (instruction.substr(7,26)));
+                    bitset<32> temp_addrs (instruction.substr(7,26));
+                    temp_instruc->addrs = to_int(temp_addrs);
                     instructions.push_back(temp_instruc);
                 }
                 else if (opcode == "101")
@@ -162,43 +165,43 @@ class Simulator
             //CATEGORY TYPE 3 instructions
             else if (format == "100")
             {
-                if(opcode = "000")
+                if(opcode == "000")
                 {
                     save_inst_16("ORI");
                 
                 }
-                else if(opcode = "001")
+                else if(opcode == "001")
                 {
                     save_inst_16("XORI");
                 
                 }
-                else if(opcode = "010")
+                else if(opcode == "010")
                 {
                     save_inst_16("ADDI");
                 
                 }
-                else if(opcode = "011")
+                else if(opcode == "011")
                 {
                     save_inst_16("SUBI");
                 
                 }
-                else if(opcode = "100")
+                else if(opcode == "100")
                 {
                     save_inst_16("ANDI");
                 
                 }
-                else if(opcode = "101")
+                else if(opcode == "101")
                 {
                     save_inst_16("SRL");
                 
                 }
             
-                else if(opcode = "110")
+                else if(opcode == "110")
                 {
                     save_inst_16("SRA");
                 
                 }
-                else if(opcode = "111")
+                else if(opcode == "111")
                 {
                     save_inst_16("SLL");
                 
@@ -209,15 +212,15 @@ class Simulator
 
         /////////////Save Break instruction insto a struct!!!
     
-        Instruc temp_instruc = new Instruc;
+        Instruc *temp_instruc = new Instruc;
         temp_instruc->type = "BREAK";
         instructions.push_back(temp_instruc);
 
         //Read the values in memory
         while(infile>>instruction)
         {
-        
-            data.push_back(to_int(bitset<32> temp_data (instruction)));
+            bitset<32> temp_data (instruction);
+            data.push_back(to_int(temp_data));
         
         }
     }
@@ -225,16 +228,17 @@ class Simulator
     void save_inst_16(string instr_name)
     {
     
-        Instruc temp_instruc = new Instruc;
+        Instruc *temp_instruc = new Instruc;
         temp_instruc->type = instr_name;
         //Get register values to retrieve numbers that will be compared to determine if there is equality
         get_reg(src1);
-        temp_instruc->src1 = to_int(bitset<5> temp_src1 (src1));
+        temp_instruc->src1 = src1;
         //get the second instruction register
 
         get_reg(src2);
-        temp_instruc->src2 = to_int(bitset<5> temp_src2 (src2));
-        temp_instruc->addrs = get_num(bitset<16> temp_addrs (instruction.substr(7,16)));
+        temp_instruc->src2 = src2;
+        bitset<32> temp_addrs (instruction.substr(7,16));
+        temp_instruc->addrs = to_int(temp_addrs);
         instructions.push_back(temp_instruc);
     
     
@@ -245,22 +249,22 @@ class Simulator
     {
     
     
-        Instruc temp_instruc = new Instruc;
+        Instruc *temp_instruc = new Instruc;
         temp_instruc->type = instr_name;
         temp_instruc->op = opcode;
         //get destination register
         get_reg(dest);
-        temp_instruc->dest = to_int(bitset<5> temp_dest (dest));
+        temp_instruc->dest = dest;
         //Get src1 from instruction and turn it into an integer value
         //this value will be the location of the register array that 
         //we will access to get the values we will perform
         //operations on
         get_reg(src1);
-        temp_instruc->src1 = to_int(bitset<5> temp_src1 (src1));
+        temp_instruc->src1 = src1;
         //get the second instruction register
 
         get_reg(src2);
-        temp_instruc->src2 = to_int(bitset<5> temp_src2 (src2));
+        temp_instruc->src2 = src2;
         instructions.push_back(temp_instruc);
     
     }
@@ -268,10 +272,10 @@ class Simulator
     {
             //cout<<format<<endl;
             
-        }
-    
-    
     }
+    
+    
+    
 
     //reset variables used to parse and analyze instruction so as to reuse them with the new incoming instructions
     void reset()
@@ -283,19 +287,20 @@ class Simulator
 
         opcode="";
 
-        dest="";
+        dest=0;
         
-        src1="";
+        src1=0;
 
-        src2="";
+        src2=0;
 
         addrs="";
     }
     //used to get bits pertaining to the register being used
-    void get_reg(string &reg)
+    void get_reg(int &reg)
 
     {
-        reg+=instruction.substr(instr_pos,5);
+        bitset<32> temp_reg(instruction.substr(instr_pos,5));
+        reg+=to_int(temp_reg);
         instr_pos+=6;
         
     
@@ -320,11 +325,6 @@ class Simulator
         //Determine what to do depending on the opcode
         if(opcode == "000")
         {
-            std::bitset<5> temp1 ;
-            std::bitset<5> temp2 ;
-            //save result from XOR into map
-            auto temp3 = temp1^=temp2;
-            map[dest] =temp3.to_string() ;
 
         }
         //else if(opcode == "001")
@@ -368,25 +368,11 @@ class Simulator
         else if (opcode == "100")
         {
         
-            std::bitset<5> temp1 ;
-            std::bitset<5> temp2 ;
-            set_bitset(temp1,temp2);
-
-            //save result from AND into map
-            auto temp3 = temp1&=temp2;
-            map[dest] =temp3;
         
         }
         else if (opcode == "101")
         {
         
-            std::bitset<5> temp1 ;
-            std::bitset<5> temp2 ;
-            //set_bitset(temp1,temp2);
-
-            //save result from OR into map
-            auto temp3 = temp1|=temp2;
-            map[dest] =temp3.to_string();
         
         }
 
@@ -395,32 +381,31 @@ class Simulator
 
     }
 
-    pair <int,int> set_bitset()
-    {
-        //retrieve register location  string ex:00000
-        get_reg(src1);
-        get_reg(src2);
-        
-        //grab value in register src1 and src2 stored in map
-        string temp_str1 = map[src1];
-        string temp_str2 = map[src2];
+    //pair <int,int> set_bitset()
+    //{
+    //    //retrieve register location  string ex:00000
+    //    get_reg(src1);
+    //    get_reg(src2);
+    //    
+    //    //grab value in register src1 and src2 stored in map
+    //    string temp_str1 = map[src1];
+    //    string temp_str2 = map[src2];
 
-        bitset<32> temp1(temp_str1);
-        bitset<32> temp2(temp_str2);
-        
-        //return 2 numbers
-        pair <int,int> pair_bit = make_pair(to_int(temp1),get_num(temp2));
-        //temp1 (temp_str1);
-        //temp2 (temp_str2);
-        return pair_bit;
-    }
+    //    bitset<32> temp1(temp_str1);
+    //    bitset<32> temp2(temp_str2);
+    //    
+    //    //return 2 numbers
+    //    pair <int,int> pair_bit = make_pair(to_int(temp1),to_int(temp2));
+    //    //temp1 (temp_str1);
+    //    //temp2 (temp_str2);
+    //    return pair_bit;
+    //}
 
     auto to_int(bitset<32> set) -> int
     {
         return static_cast<int>(set.to_ulong());
     
     }
-
 
 
 
